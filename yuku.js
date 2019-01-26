c0110ee3b8ccbe24b36727f4951b9fcc028df362
yukudiscord <46381846+yukudiscord@users.noreply.github.com>
2019-01-26 12:16:11 +0200

    Update yuku.js

--- yuku.js
+++ yuku.js
@@ -6,10 +6,6 @@ c="#ddfffa"
 
 client.login(process.env.TOKEN);
 
-client.on('message', message => {
-    const args = message.content.slice(p.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-if(message.channel.id === '529339985837555752'){message.react("<:love:537673302383263765>")}
 if (message.content.startsWith(p + `eval`) && (message.author.id === "406343162651738112" || message.author.id === "341988428457705482")) {
     const code = message.content.split(" ").slice(1).join(" ");
     try {
