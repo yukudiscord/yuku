1a93b501d34cd1f8610763433cc3a3929d37ba4a
Z9nn <36812758+cykable@users.noreply.github.com>
2019-01-26 12:34:38 +0300

    Update yuku.js

--- yuku.js
+++ yuku.js
@@ -9,7 +9,7 @@ client.login(process.env.TOKEN);
 client.on('message', message => {
     const args = message.content.slice(p.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-if(message.channel.id === '529339985837555752'){message.react(':love:')}
+if(message.channel.id === '529339985837555752'){message.react('❤️')}
 if (message.content.startsWith(p + `eval`) && (message.author.id === "406343162651738112" || message.author.id === "341988428457705482")) {
     const code = message.content.split(" ").slice(1).join(" ");
     try {
